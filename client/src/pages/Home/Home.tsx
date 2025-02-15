7b5713cf5243885845d108e6583390e23051f6d8
Joshua Byrd <jsbyrd02@gmail.com>
2025-02-15 18:58:26 -0500

    Change front page links

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import ChessboardWrapper from "@/components/ChessboardWrapper";
 import { DEFAULT_POSITION } from "chess.js";
 import { Chessboard } from "react-chessboard";
-import { Pen, Bot } from "lucide-react";
+import { Pen, Bot, Square } from "lucide-react";
 import { NavLink } from "react-router";
 
 const primaryButtonStyling: string =
@@ -22,29 +22,29 @@ const Home = () => {
         </h1>
         <div className="flex flex-col items-center gap-5 justify-around h-full">
           <NavLink
-            to="/coordinates"
+            to="/coordinates/search-square"
             className={`${primaryButtonStyling} flex items-center p-4 gap-6 block min-w-80 max-w-[30rem] w-full h-28 rounded-lg`}
           >
-            <Pen className="w-16 h-16" strokeWidth={2} />
+            <Square className="w-16 h-16" strokeWidth={2} />
             <div className="w-full h-full flex flex-between gap-2 items-center">
               <div className="flex flex-col gap-2">
-                <h2 className="text-3xl font-bold">Coordinates</h2>
+                <h2 className="text-3xl font-bold">Search Square</h2>
                 <p className="text-wrap line-clamp-2">
-                  Learn to recognize chess coordinates instantly!
+                  Get better at recognizing chess coordinates!
                 </p>
               </div>
             </div>
           </NavLink>
           <NavLink
-            to="/coordinates"
+            to="/coordinates/name-notation"
             className={`${secondaryButtonStyling} flex items-center p-4 gap-6 block min-w-80 max-w-[30rem] w-full h-28 rounded-lg`}
           >
-            <Bot className="w-16 h-16" strokeWidth={2} />
+            <Pen className="w-16 h-16" strokeWidth={2} />
             <div className="w-full h-full flex flex-between gap-2 items-center">
               <div className="flex flex-col gap-2">
-                <h2 className="text-3xl font-bold">AI Analysis</h2>
+                <h2 className="text-3xl font-bold">Name Notation</h2>
                 <p className="text-wrap line-clamp-2">
-                  Analyze your games with the help of chess engines!
+                  Name moves in chess notation as fast as possible!
                 </p>
               </div>
             </div>
