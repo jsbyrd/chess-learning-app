16d25dc8beb205ad1b263109d8547c648c27c665
Joshua Byrd <jsbyrd02@gmail.com>
2025-02-17 17:35:07 -0500

    Exclude GET '/' from the /api global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
